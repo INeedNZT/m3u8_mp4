ecb16b5a48d76ce4a094fd00dd31ae33ef5b3a4a
INeedNZT <554214611@qq.com>
2024-02-12 12:59:01 +0000

    add workspace for m3u8_png_mp4

--- m3u8_png_mp4.sh
+++ m3u8_png_mp4.sh
@@ -8,13 +8,26 @@ TS_DIR="ts"
 MAX_JOB_COUNT=35
 MAX_ATTEMPTS=5
 
+# 初始化存储空间
+init_workspace() {
+    echo "请输入存储空间的路径（将在此目录下保存所有文件）:"
+    read workspace
+    mkdir -p "$workspace"
+    echo "存储空间设置为:$workspace"
+}
+
+# 初始化下载文件目录
+init_tmp_workspace(){
+    tmp_workspace="$workspace/$output_file"
+    # 输出文件名的文件夹
+    mkdir -p "$tmp_workspace"
+    echo "已创建临时下载任务目录: $(pwd "$tmp_workspace")" >> "$tmp_workspace/log.txt"
+}
+
 # 清理文件函数
 cleanup_files() {
   echo "正在清理文件..."
-  rm -rf "$DOWNLOAD_DIR"
-  rm -rf "$TS_DIR"
-  rm -f failed_downloads.txt
-  rm -f log.txt
+  rm -rf "$tmp_workspace"
 }
 
 # 中止进程
@@ -24,27 +37,29 @@ cleanup_subprocesses() {
   kill $(jobs -p) 2>/dev/null
 }
 
-# 设置信号处理
-trap cleanup_subprocesses SIGINT SIGTERM
+exit_and_cleanup() {
+    cleanup_subprocesses
+    cleanup_files
+    echo "正在退出脚本..."
+    exit
+}
 
 # 主要下载逻辑
 download() {
-  M3U8_URL=$1  # M3U8链接
-  FINAL_VIDEO=$2  # 输出视频文件名
-  base_url=$(echo $M3U8_URL | sed -E 's|(https?://[^/]+).*|\1|')
+  base_url=$(echo $m3u8_url | sed -E 's|(https?://[^/]+).*|\1|')
   # 清理旧文件
   cleanup_files
 
   # 下载M3U8文件
-  mkdir -p "$DOWNLOAD_DIR"
-  m3u8_file="$DOWNLOAD_DIR/playlist.m3u8"
-  curl "$M3U8_URL" -o "$m3u8_file"
+  mkdir -p "$tmp_workspace/$DOWNLOAD_DIR"
+  m3u8_file="$tmp_workspace/$DOWNLOAD_DIR/playlist.m3u8"
+  curl "$m3u8_url" -o "$m3u8_file"
 
   # 下载和转换PNG文件
-  mkdir -p "$TS_DIR"
-  png_urls=$(grep -E '^[^#].*[^[:space:]]' "$DOWNLOAD_DIR/playlist.m3u8")
+  mkdir -p "$tmp_workspace/$TS_DIR"
+  png_urls=$(grep -E '^[^#].*[^[:space:]]' "$tmp_workspace/$DOWNLOAD_DIR/playlist.m3u8")
 
-  echo "发现: $(echo "$png_urls" | wc -l) 个资源文件需要下载" >> log.txt
+  echo "发现: $(echo "$png_urls" | wc -l) 个资源文件需要下载" >> $tmp_workspace/log.txt
 
   for url in $png_urls; do
     if [[ ! "$url" =~ ^http ]]; then
@@ -52,11 +67,11 @@ download() {
     fi
     
     filename=$(basename "$url")
-    png_file="$DOWNLOAD_DIR/$filename"
-    ts_file="$TS_DIR/${filename}.ts"
+    png_file="$tmp_workspace/$DOWNLOAD_DIR/$filename"
+    ts_file="$tmp_workspace/$TS_DIR/${filename}.ts"
 
     while [ $(jobs -p | wc -l) -ge $MAX_JOB_COUNT ]; do
-      # echo "已达到最大后台进程数，休眠1秒后重试" >> log.txt
+      # echo "已达到最大后台进程数，休眠1秒后重试" >> "$tmp_workspace/log.txt"
       sleep 1
     done
     
@@ -64,16 +79,17 @@ download() {
       attempts=0
       while [ $attempts -lt $MAX_ATTEMPTS ]; do
         if curl -L "$url" -o "$png_file" && [ -s "$png_file" ]; then
-            echo "下载 $url 成功" >> log.txt
-            echo "正在转换 $filename 到ts文件..." >> log.txt
+            echo "下载 $url 成功" >> "$tmp_workspace/log.txt"
+            echo "正在转换 $filename 到ts文件..." >> "$tmp_workspace/log.txt"
             dd if="$png_file" of="$ts_file" bs=4 skip=53
+            # 验证是否是ts文件，有时候可能是被封了，返回的是html文件或是其他格式的文件
             if ffprobe -v error -show_format -i "$ts_file" 2>&1 | grep -q "format_name=mpegts"; then
-              echo "转换文件 $filename 成功" >> log.txt
+              echo "转换文件 $filename 成功" >> "$tmp_workspace/log.txt"
               break
             fi
-            echo "转换ts文件失败，可能不是ts文件，详情去ts目录下查看 $ts_file 是否存在以及它的内容" >> log.txt
+            echo "转换ts文件失败，可能不是ts文件，详情去ts目录下查看 $ts_file 是否存在以及它的内容" >> "$tmp_workspace/log.txt"
         fi
-        echo "第 $((attempts+1)) 次尝试下载或转换失败，5秒后重试" >> log.txt
+        echo "第 $((attempts+1)) 次尝试下载或转换失败，5秒后重试" >> "$tmp_workspace/log.txt"
         sleep 5
         attempts=$((attempts+1))
       done
@@ -87,42 +103,45 @@ download() {
   wait
 
   # 先把原始的m3u8文件复制一份到ts目录下
-  cp "$DOWNLOAD_DIR/playlist.m3u8" "$TS_DIR/playlist.m3u8"
+  cp "$tmp_workspace/$DOWNLOAD_DIR/playlist.m3u8" "$tmp_workspace/$TS_DIR/playlist.m3u8"
   # 替换URL为本地文件名
   for url in $png_urls; do
     filename=$(basename "$url").ts
-    awk -v old="$url" -v new="$filename" '{gsub(old, new); print}' "$TS_DIR/playlist.m3u8" > tmp.m3u8 && mv tmp.m3u8 "$TS_DIR/playlist.m3u8"
+    awk -v old="$url" -v new="$filename" '{gsub(old, new); print}' "$tmp_workspace/$TS_DIR/playlist.m3u8" > tmp.m3u8 && mv tmp.m3u8 "$tmp_workspace/$TS_DIR/playlist.m3u8"
   done
 
-  echo "已下载和转换所有ts文件，正在合并文件..." >> log.txt
+  echo "已下载和转换所有ts文件，正在合并文件..." >> "$tmp_workspace/log.txt"
 
-  if ffmpeg -i "$TS_DIR/playlist.m3u8" -c copy "$FINAL_VIDEO"; then
-    echo "最终视频 $FINAL_VIDEO 创建成功" >> log.txt
+  if ffmpeg -i "$tmp_workspace/$TS_DIR/playlist.m3u8" -c copy "$workspace/$output_file.mp4"; then
+    echo "最终视频 $output_file.mp4 创建成功" >> "$tmp_workspace/log.txt"
     cleanup_files
   else
-    echo "创建视频失败." >> log.txt
+    echo "创建视频失败." >> "$tmp_workspace/log.txt"
   fi
 }
 
+# 初始化存储空间
+init_workspace
 
+# 设置信号处理
+trap exit_and_cleanup SIGINT SIGTERM
 
 while true; do
   # 用户交互，选择操作
-  echo "请选择操作："
+  echo "请选择操作:"
   echo "1. 创建下载任务"
-  echo "2. 退出并清理"
-  read -p "请输入选项（1/2）：" option
+  echo "2. 退出"
+  read -p "请输入选项（1/2）:" option
   # 根据用户选择执行相应操作
   case $option in
     1)
-      read -p "请输入M3U8 URL：" m3u8_url
-      read -p "请输入输出视频文件名：" output_video
-      download "$m3u8_url" "$output_video"
+      read -p "请输入M3U8 URL:" m3u8_url
+      read -p "请输入输出文件名（不需要mp4后缀）:" output_file
+      init_tmp_workspace 
+      download
       ;;
     2)
-      cleanup_files
-      echo "已清理临时文件，程序退出。"
-      exit 0
+      exit_and_cleanup
       ;;
     *)
       echo "无效选项，程序退出。"
