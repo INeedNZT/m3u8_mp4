c5791f053387b10ff46bbd45f9b825ea63aa8fee
INeedNZT <554214611@qq.com>
2024-02-12 13:24:01 +0000

    fix print issue

--- m3u8_png_mp4.sh
+++ m3u8_png_mp4.sh
@@ -113,7 +113,7 @@ download() {
   echo "已下载和转换所有ts文件，正在合并文件..." >> "$tmp_workspace/log.txt"
 
   if ffmpeg -i "$tmp_workspace/$TS_DIR/playlist.m3u8" -c copy "$workspace/$output_file.mp4" >> "$tmp_workspace/log.txt" 2>&1; then
-    echo "视频下载成功！文件保存在 $(pwd "$tmp_workspace/$output_file.mp4")"
+    echo "视频下载成功！文件保存在 $(pwd "$workspace/$output_file.mp4")"
     cleanup_files
   else
     echo "视频合并失败" >> "$tmp_workspace/log.txt"
